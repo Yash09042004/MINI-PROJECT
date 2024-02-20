e739b2c4bfcf7064bace9dd81c9745c4d44810ff
Yash09042004 <yashkiran2004@gmail.com>
2024-02-20 16:14:20 +0530

    commited

--- one.cpp
+++ one.cpp
@@ -8,7 +8,7 @@ cin>>a>>b;
 cout<<"Multiplication is : "<<a*b<<endl;
 
 cout<<"changes from main"<<endl;
-
+//ewwwwwwwwwwwwwwwwwwwwwwwwwwww
 cout<<"changing something"<<endl;
 couut<<"newwwwwwwwwwwwww"<<endl;
 return 0;
